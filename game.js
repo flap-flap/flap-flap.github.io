25faf11f41274efa90ac457e6acc4d0a2aee35e1
flap-flap <78896901+flap-flap@users.noreply.github.com>
2021-02-14 15:00:19 +0800

    Update game.js

--- game.js
+++ game.js
@@ -86,7 +86,7 @@ function draw() {
 
  ctx.fillStyle = "#000";
  ctx.font = "24px Verdana";
- ctx.fillText("членоёбчик: " + score, 10, cvs.height - 20);
+ ctx.fillText("Score: " + score, 10, cvs.height - 20);
 
  requestAnimationFrame(draw);
 }
